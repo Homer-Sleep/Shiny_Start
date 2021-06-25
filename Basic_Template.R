7279cbfcacaf79937d65105b9d77dd3b67d549f8
Dave <ryand1@goldmail.etsu.edu>
2021-06-24 23:02:19 -0400

    Just started

---
+++ Basic_Template.R
@@ -0,0 +1,9 @@
+library(shiny)
+
+ui = fluidPage("I made this Shit!")
+
+server = function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+# From https://shiny.rstudio.com/tutorial/ --8:21
